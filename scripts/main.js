ef2781e8486a859d982554c2b7611d60b459c304
ANnamdi <mikeadossi@gmail.com>
2016-02-13 15:31:44 -0600

    skills section added

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+$(document).ready(function(){    
+
+var dHeight = $(this).height()-$(window).height();
+  if (dHeight >= $(this).scrollTop()) {
+    $('nav').css('background', 'rgba(53,145,204,' + $(this).scrollTop() / dHeight + ')');
+  }
+
+});
