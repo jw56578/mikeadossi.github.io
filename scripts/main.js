96e595a48c01c528b61e33d79acf82ace22dde89
ANnamdi <mikeadossi@gmail.com>
2016-02-16 15:14:43 -0600

    styling and anchor referencing changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
-$(document).ready(function(){    
+$(document).ready(function(){
 
-var dHeight = $(this).height()-$(window).height();
+var scrollTopValue;
+var portfolioDiv = $('#portfolioDiv')
+var updatePosition = function(){
+	console.log(portfolioDiv.position());
+	console.log($(window).scrollTop())
+	scrollTopValue = $(window).scrollTop()
+
+	if(scrollTopValue >= 382){
+		$('#nav').css('background-color','steelblue');
+		$('.navItem').css('color','white')
+	} else{
+		$('#nav').css('background-color','transparent');
+		$('.navItem').css('color','lightgrey')
+	}
+}
+
+var throttled = _.throttle(updatePosition, 300)
+
+window.onscroll = throttled;
+
+/*var dHeight = $(this).height()-$(window).height();
   if (dHeight >= $(this).scrollTop()) {
     $('nav').css('background', 'rgba(53,145,204,' + $(this).scrollTop() / dHeight + ')');
   }
-
+*/
 });
